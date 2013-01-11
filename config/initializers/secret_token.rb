2cc601b882c26723aa18259e282d7c4357367d1b
Zainul Shah <zan2434@gmail.com>
2013-01-11 15:27:06 -0800

    new initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snaproulette::Application.config.secret_token = '55e7551182da2746fcc00c71a14fd110f9c9dbe29b08a403eaf8eb8ff629c2b892cc53534a60f8b2ef3d145d9ba0a9cf38d7692c5225436bd069e11d6d1e6eeb'
