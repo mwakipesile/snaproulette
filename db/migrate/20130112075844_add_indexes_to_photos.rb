bad44389a55714d857ac97600dccf21fdae61afc
Zainul Shah <zan2434@gmail.com>
2013-01-12 00:13:56 -0800

    Quick fix

--- db/migrate/20130112075844_add_indexes_to_photos.rb
+++ db/migrate/20130112075844_add_indexes_to_photos.rb
@@ -1,7 +1,6 @@
 class AddIndexesToPhotos < ActiveRecord::Migration
   def change
-  	add_index :photos, :recipient_id
-  	add_index :photos, :original_photo_id
   	add_column :photos, :original_photo_id, :integer
+  	add_index :photos, :original_photo_id
   end
 end
